1c5b9ec16085b0f99244f16ee91d3b7baac07fe6
Abdulla Vennakode <abdullacp9048@gmail.com>
2026-02-03 16:35:01 +0530

    repaired date

--- Frontend/src/Components/ItemCard.jsx
+++ Frontend/src/Components/ItemCard.jsx
@@ -1,33 +1,31 @@
+import { useMemo } from 'react';
 import { useState } from 'react';
-import { useEffect } from 'react';
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
 
 const ItemCard = ({item, image, location, state, country, date, type, id}) => {
-    console.log(date)
+    const [now] = useState(() => Date.now())
 
-    const [timeAgo , setTimeAgo] = useState();
+    const timeAgo = useMemo(() => {
+        const msPerMinute = 60 * 1000;
+        const msPerHour = msPerMinute * 60;
+        const msPerDay = msPerHour * 24;
+        const currValue = now - new Date(date).getTime();
 
-    useEffect(() => {
-        const findTimeAgo = () => {
-            const msPerMinute = 60 * 1000;
-            const msPerHour = msPerMinute * 60;
-            const msPerDay = msPerHour * 24;
-            const currValue = Date.now() - Number(date);
 
-            if(currValue < msPerMinute) return "Just Now"
-            if(currValue < msPerHour) return Math.round(currValue / msPerMinute) + ` Minute ago`;
-            if(currValue < msPerDay) return Math.round(currValue / msPerHour) + ` Hour ago`;
-            return Math.round(currValue / msPerDay) + "Days ago"
-        }
-        setTimeAgo(findTimeAgo())
-    },[date])
+        if(currValue < msPerMinute) return "Just Now"
+        if(currValue < msPerHour) return Math.round(currValue / msPerMinute) + ` Minute ago`;
+        if(currValue < msPerDay) return Math.round(currValue / msPerHour) + ` Hour ago`;
+        return Math.round(currValue / msPerDay) + "Days ago"
+        
+    },[date, now])
+
 
   return (
     <div className='w-71 rounded-3xl p-3 relative bg-white'>
       <div className='absolute bg-green-600 text-white top-5 right-5 px-3 text-sm rounded'>{type}</div>
       <div className='h-43 rounded-2xl w-full overflow-hidden bg-gray-100'>
-        <img src={`${image}`} alt={`${image}`} className='h-45'/>
+        <img src={`${image}`} alt="" className='h-45'/>
       </div>
       <div className='mt-3 w-full'>
         <p className='text-2xl font-bold'>{item}</p>
