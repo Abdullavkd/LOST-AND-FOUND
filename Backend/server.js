c20da4729a4cb08f2326b229aecdc05c223263ec
Abdulla Vennakode <abdullacp9048@gmail.com>
2026-02-11 19:51:26 +0530

    changed api.js code for access token and refresh token

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true               
+}));
+
 app.use(express.json())
 
 app.use("/", authRouter)
--- Frontend/src/Services/api.js
+++ Frontend/src/Services/api.js
@@ -1,16 +1,38 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000' // backend server port
+    baseURL: 'http://localhost:4000', // backend server port
+    withCredentials: true
 });
 
-// this interceptor automatically add jwt to every requests
-api.interceptors.request.use((config) => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if(user && user.token) {
-        config.headers.Authorization = user.token;
+api.interceptors.request.use((response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        
+        // if error is 401
+        if(error.response.status ===401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                await axios.get('http://localhost:4000/refresh',{withCredentials: true})
+                return api(originalRequest)
+            } catch (refreshError) {
+                window.location.href('/login')
+                return Promise.reject(refreshError)
+            }
+        }
+        return Promise.reject(error)
     }
-    return config;
-})
+)
+
+
+// // this interceptor automatically add jwt to every requests
+// api.interceptors.request.use((config) => {
+//     const user = JSON.parse(localStorage.getItem('user'));
+//     if(user && user.token) {
+//         config.headers.Authorization = user.token;
+//     }
+//     return config;
+// })
 
 export default api;
